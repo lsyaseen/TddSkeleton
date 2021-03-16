bb3e7fb608b0344cec937bfcc0887bcb58907c88
Lama Yaseen <lsyaseens@gmail.com>
2021-03-16 16:55:54 +0300

    implement the return null for getItem

--- src/main/java/com/oocode/Example.java
+++ src/main/java/com/oocode/Example.java
@@ -11,15 +11,16 @@ public class Example {
     }
 
     public void addItem(String a) {
-        if (!aList.contains(a)) {
-            aList.add(0, a);
-        } else {
+        if (aList.contains(a)) {
             aList.remove(a);
-            aList.add(0, a);
         }
+        aList.add(0, a);
     }
 
     public String getItem(int i) {
+        if (i >= aList.size()){
+            return null;
+        }
         return aList.get(i);
     }
 
