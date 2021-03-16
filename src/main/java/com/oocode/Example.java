f29c77ec3377d9f37b9262b858925e3ae3149658
mba <mba2019@gmx.de>
2021-03-16 14:43:34 +0100

    getItem implementation

--- src/main/java/com/oocode/Example.java
+++ src/main/java/com/oocode/Example.java
@@ -11,16 +11,16 @@ public class Example {
     }
 
     public void addItem(String a) {
-        if (!aList.contains(a)){
-            aList.add(0,a);
+        if (!aList.contains(a)) {
+            aList.add(0, a);
         } else {
-             aList.remove(a);
-             aList.add(0, a);
+            aList.remove(a);
+            aList.add(0, a);
         }
     }
 
-    public void getItem(int i){
-
+    public String getItem(int i) {
+        return aList.get(i);
     }
 
     public boolean isEmpty() {
