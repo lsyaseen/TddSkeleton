2de210d690bb8143125ac18632ce513a14e7d5c1
Lama Yaseen <lsyaseens@gmail.com>
2021-03-16 14:11:16 +0300

    Added empty list

--- src/main/java/com/oocode/Example.java
+++ src/main/java/com/oocode/Example.java
@@ -1,7 +1,13 @@
 package com.oocode;
+import java.util.*;
 
 public class Example {
-	public int answer() {
-		return 42;	
+
+	public static void main(String args[]){
+
+		List<String> aList = new ArrayList<String>();
+
 	}
+
+
 }
